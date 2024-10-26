a088cc12ff23ae71307083fc88604c8ab0c52eab
BinhNV38 <binhnv38@fpt.com>
2024-10-26 21:17:23 +0700

    Loginnn

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,8 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Route;
-use Auth;
-use Hash;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
