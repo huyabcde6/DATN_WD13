860625ebdca0519fbbf0452b7b424119c76efb88
Binhnv894 <binhnvph33903@fpt.edu.vn>
2024-11-26 23:05:10 +0700

    anc

---
+++ resources/views/role-permission/nav-links.blade.php
@@ -0,0 +1,6 @@
+<div class="container mt-3">
+    <a href="{{ url('roles') }}" class="btn btn-primary mx-2">Roles</a>
+    <a href="{{ url('permission') }}" class="btn btn-success mx-2">Permission</a>
+    <a href="{{ url('users') }}" class="btn btn-danger mx-2">Users</a>
+
+</div>
